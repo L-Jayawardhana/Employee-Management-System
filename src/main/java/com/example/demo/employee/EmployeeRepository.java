66ad95bd08f192987611324eeae114fe0622db85
L-Jayawardhana <lakdinurameshjayawardhana@gmail.com>
2025-05-08 22:31:21 +0530

    Update EmployeeRepository.java

--- src/main/java/com/example/demo/employee/EmployeeRepository.java
+++ src/main/java/com/example/demo/employee/EmployeeRepository.java
@@ -1,13 +1,16 @@
 package com.example.demo.employee;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    List<Employee> findByName(String name);
+    @Query("SELECT e FROM Employee e WHERE e.first_name = :first_name")
+    List<Employee> findByFirst_name(@Param("first_name") String first_name);
     Optional<Employee> findByEmail(String email);
 }
