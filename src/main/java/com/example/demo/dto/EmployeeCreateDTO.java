2755b0760a534d391e3e9a76810a8b14d91c3645
L-Jayawardhana <lakdinurameshjayawardhana@gmail.com>
2025-05-28 03:23:13 +0530

    Create EmployeeCreateDTO.java
    add dto

---
+++ src/main/java/com/example/demo/dto/EmployeeCreateDTO.java
@@ -0,0 +1,43 @@
+package com.example.demo.dto;
+
+public class EmployeeCreateDTO {
+    private String firstName;
+    private String lastName;
+    private String nic;
+    private String gender;
+    private String phone;
+    private String email;
+    private String password;
+    private String birthday;
+    private String departmentId;
+
+    public EmployeeCreateDTO() {}
+
+    // Getters and setters
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public String getNic() { return nic; }
+    public void setNic(String nic) { this.nic = nic; }
+
+    public String getGender() { return gender; }
+    public void setGender(String gender) { this.gender = gender; }
+
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getBirthday() { return birthday; }
+    public void setBirthday(String birthday) { this.birthday = birthday; }
+
+    public String getDepartmentId() { return departmentId; }
+    public void setDepartmentId(String departmentId) { this.departmentId = departmentId; }
+}
