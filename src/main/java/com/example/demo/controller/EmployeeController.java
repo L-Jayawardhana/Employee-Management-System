e2085e06039b9db5cd73be7564f4ace372db9bb8
L-Jayawardhana <lakdinurameshjayawardhana@gmail.com>
2025-06-25 23:57:55 +0530

    finalize EmployeeController

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -6,10 +6,9 @@ import com.example.demo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/employee")
@@ -32,4 +31,32 @@ public class EmployeeController {
         }
     }
 
+    @GetMapping
+    public ResponseEntity<?> getAllEmployees() {
+        try{
+            List<EmployeeResponseDTO> employees = employeeService.getAllEmployees();
+            return ResponseEntity.status(HttpStatus.OK).body(employees);
+        }catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getEmployeeById(@PathVariable String id) {
+        try{
+            EmployeeResponseDTO employee = employeeService.getEmployeeById(id);
+            return ResponseEntity.status(HttpStatus.FOUND).body(employee);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Employee not found with id: " + id + e.getMessage());
+        }
+    }
+
+    public ResponseEntity<?> getEmployeesByDepartmentId(@PathVariable String id) {
+        try{
+            List<EmployeeResponseDTO> employees = employeeService.getEmployeesByDepartmentId(id);
+            return ResponseEntity.status(HttpStatus.FOUND).body(employees);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Department not found with id: " + id + e.getMessage());
+        }
+    }
 }
