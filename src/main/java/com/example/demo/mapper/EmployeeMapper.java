e8d0c58dd16128b78c6df83cf20ed4e6ece89ab1
L-Jayawardhana <lakdinurameshjayawardhana@gmail.com>
2025-06-27 02:21:31 +0530

    Update EmployeeMapper according to the DTOs

--- src/main/java/com/example/demo/mapper/EmployeeMapper.java
+++ src/main/java/com/example/demo/mapper/EmployeeMapper.java
@@ -6,24 +6,22 @@ import com.example.demo.model.Department;
 import com.example.demo.model.Employee;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
-
 @Component
 public class EmployeeMapper {
 
-    public Employee toEntity(EmployeeCreateDTO dto, Department department, String hashedPassword) {
+    public Employee toEntity(EmployeeCreateDTO dto, Department department, String hashedPassword, int age) {
         Employee employee = new Employee();
 
         employee.setFirst_name(dto.getFirstName());
         employee.setLast_name(dto.getLastName());
         employee.setNic(dto.getNic());
+        employee.setAddress(dto.getAddress());
         employee.setGender(dto.getGender());
         employee.setPhone(dto.getPhone());
         employee.setEmail(dto.getEmail());
         employee.setPassword(hashedPassword);
-        if (dto.getBirthday() != null && !dto.getBirthday().isEmpty()) {
-            employee.setBirthday(LocalDate.parse(dto.getBirthday()));
-        }
+        employee.setAge(age);
+        employee.setBirthday(dto.getBirthday());
         employee.setDepartment(department);
 
         return employee;
@@ -34,13 +32,11 @@ public class EmployeeMapper {
         dto.setId(savedEmployee.getId());
         dto.setFirstName(savedEmployee.getFirst_name());
         dto.setLastName(savedEmployee.getLast_name());
-        dto.setNic(savedEmployee.getNic());
+        dto.setAddress(savedEmployee.getAddress());
         dto.setPhone(savedEmployee.getPhone());
         dto.setEmail(savedEmployee.getEmail());
         dto.setAge(savedEmployee.getAge());
-        if (savedEmployee.getDepartment() != null) {
-            dto.setDepartmentId(savedEmployee.getDepartment().getId());
-        }
+        dto.setDepartmentId(savedEmployee.getDepartment().getId());
         return dto;
     }
 }
