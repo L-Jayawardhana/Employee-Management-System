387a35db7e24c6f52daa01465a46da3c2ca29e64
L-Jayawardhana <lakdinurameshjayawardhana@gmail.com>
2025-07-01 01:09:44 +0530

    add exception for employee already in the database

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -14,12 +14,6 @@ public class GlobalExceptionHandler {
     // You can define methods annotated with @ExceptionHandler to handle specific exceptions.
     // For example, you can handle ResourceNotFoundException or any other custom exceptions here.
 
-    @ExceptionHandler(DepartmentNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleDepartmentNotFound(DepartmentNotFoundException e) {
-        ErrorResponse errorResponse = new ErrorResponse(e.getMessage(), 404, LocalDateTime.now());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
-    }
-
     @ExceptionHandler(EmployeeNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleEmployeeNotFound(EmployeeNotFoundException e) {
         ErrorResponse errorResponse = new ErrorResponse(e.getMessage(), 404, LocalDateTime.now());
@@ -32,9 +26,37 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 
+    @ExceptionHandler(EmployeeAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponse> handleEmployeeAlreadyExists(EmployeeAlreadyExistsException e) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage(), 409, LocalDateTime.now());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
+    }
+
+    //throws when a department is not found in the system but system is not empty
+    @ExceptionHandler(DepartmentNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleDepartmentNotFound(DepartmentNotFoundException e) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage(), 404, LocalDateTime.now());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+    //throws when no departments are found in the system (system is empty)
+    @ExceptionHandler(NoDepartmentsFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNoDepartmentsFound(NoDepartmentsFoundException e) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage() + " | happens 204 NO_CONTENT error", 204, LocalDateTime.now());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+    @ExceptionHandler(DepartmentAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponse> handleDepartmentAlreadyExists(DepartmentAlreadyExistsException e) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage(), 409, LocalDateTime.now());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGenericException(Exception ex) {
         ErrorResponse errorResponse = new ErrorResponse( "Internal Server Error: " + ex.getMessage(),500, LocalDateTime.now());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
     }
+
+
 }
