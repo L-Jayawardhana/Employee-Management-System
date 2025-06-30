6abc0b3348d8563256d245bc67b6619e52886287
L-Jayawardhana <lakdinurameshjayawardhana@gmail.com>
2025-06-30 23:44:07 +0530

    Modify name and content

--- src/main/java/com/example/demo/dto/DepartmentResponseDTO.java
+++ src/main/java/com/example/demo/dto/DepartmentResponseDTO.java
@@ -1,17 +1,9 @@
 package com.example.demo.dto;
 
-public class DepartmentDTO {
+public class DepartmentResponseDTO {
     private String id;
     private String name;
     private long salary;
-
-    public DepartmentDTO() {}
-
-    public DepartmentDTO(String id, String name, long salary) {
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
-    }
 
     public String getId() { return id; }
     public void setId(String id) { this.id = id; }
