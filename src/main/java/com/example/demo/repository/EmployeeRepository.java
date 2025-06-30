d084dcb92f09410609376c0e3d91cf713a1b9ba6
L-Jayawardhana <lakdinurameshjayawardhana@gmail.com>
2025-07-01 01:12:17 +0530

    delete not using things

--- src/main/java/com/example/demo/repository/EmployeeRepository.java
+++ src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -12,11 +12,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, String> {
-    @Query("SELECT e FROM Employee e WHERE e.first_name = :first_name")
-    List<Employee> findByFirst_name(@Param("first_name") String first_name);
-
-    Optional<Employee> findByEmail(String email);
-
     @Query("SELECT MAX(CAST(SUBSTRING(e.id, LENGTH(e.department.id) + 1) AS int)) FROM Employee e WHERE e.department = :department")
     Integer findMaxIdNumberByDepartment(@Param("department") Department department);
 
