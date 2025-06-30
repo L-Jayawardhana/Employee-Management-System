e36952ef2dbaec465763760616f4b0f8dd205568
L-Jayawardhana <lakdinurameshjayawardhana@gmail.com>
2025-06-30 23:44:16 +0530

    Modify content

--- src/main/java/com/example/demo/dto/DepartmentCreateDTO.java
+++ src/main/java/com/example/demo/dto/DepartmentCreateDTO.java
@@ -4,13 +4,6 @@ public class DepartmentCreateDTO {
     private String name;
     private long salary;
 
-    public DepartmentCreateDTO() {}
-
-    public DepartmentCreateDTO(String name, long salary) {
-        this.name = name;
-        this.salary = salary;
-    }
-
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
     
