859a9110e93dbbd46d3db6cbd9d6f3f66c3ef8ff
L-Jayawardhana <lakdinurameshjayawardhana@gmail.com>
2025-07-11 23:30:48 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,11 @@
 package com.example.demo;
 
+import com.example.demo.model.Department;
+import com.example.demo.repository.DepartmentRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import com.example.demo.model.Department;
-import com.example.demo.repository.DepartmentRepository;
 
 @SpringBootApplication
 public class DemoApplication {
