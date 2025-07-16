4cfd949b65df20bfd037acbd3ad56f16f82addc1
L-Jayawardhana <lakdinurameshjayawardhana@gmail.com>
2025-07-17 03:16:18 +0530

    Update DepartmentResponseDTO.java

--- src/main/java/com/example/demo/dto/DepartmentResponseDTO.java
+++ src/main/java/com/example/demo/dto/DepartmentResponseDTO.java
@@ -4,6 +4,7 @@ public class DepartmentResponseDTO {
     private String id;
     private String name;
     private long salary;
+    private int OverTimeRate;
 
     public String getId() { return id; }
     public void setId(String id) { this.id = id; }
@@ -13,4 +14,12 @@ public class DepartmentResponseDTO {
     
     public long getSalary() { return salary; }
     public void setSalary(long salary) { this.salary = salary; }
+
+    public int getOverTimeRate() {
+        return OverTimeRate;
+    }
+
+    public void setOverTimeRate(int overTimeRate) {
+        OverTimeRate = overTimeRate;
+    }
 }
