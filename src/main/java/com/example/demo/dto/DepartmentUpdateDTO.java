8b78028f5aafea20ca2b332ceeea994587c90ee5
L-Jayawardhana <lakdinurameshjayawardhana@gmail.com>
2025-07-17 03:16:23 +0530

    Update DepartmentUpdateDTO.java

--- src/main/java/com/example/demo/dto/DepartmentUpdateDTO.java
+++ src/main/java/com/example/demo/dto/DepartmentUpdateDTO.java
@@ -2,6 +2,7 @@ package com.example.demo.dto;
 
 public class DepartmentUpdateDTO {
     private long salary;
+    private int OverTimeRate;
 
     public DepartmentUpdateDTO(long salary) {
         this.salary = salary;
@@ -13,5 +14,13 @@ public class DepartmentUpdateDTO {
 
     public void setSalary(long salary) {
         this.salary = salary;
+    }
+
+    public int getOverTimeRate() {
+        return OverTimeRate;
+    }
+
+    public void setOverTimeRate(int overTimeRate) {
+        OverTimeRate = overTimeRate;
     }
 }
