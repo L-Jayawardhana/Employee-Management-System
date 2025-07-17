877bf54dcb62d5d6b15330fa264750467dd30949
L-Jayawardhana <lakdinurameshjayawardhana@gmail.com>
2025-07-18 00:35:11 +0530

    Add OverTimeRate field to DepartmentCreateDTO
    Introduced OverTimeRate property with getter and setter methods to DepartmentCreateDTO to support overtime rate data in department creation.

--- src/main/java/com/example/demo/dto/DepartmentCreateDTO.java
+++ src/main/java/com/example/demo/dto/DepartmentCreateDTO.java
@@ -3,6 +3,17 @@ package com.example.demo.dto;
 public class DepartmentCreateDTO {
     private String name;
     private long salary;
+    private int OverTimeRate;
+
+    public int getOverTimeRate() {
+        return OverTimeRate;
+    }
+
+    public void setOverTimeRate(int overTimeRate) {
+        OverTimeRate = overTimeRate;
+    }
+
+
 
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
